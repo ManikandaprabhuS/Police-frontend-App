547bb35adb4326205327c4e31a7cc41dfdab6261
Manikandaprabhu S <your-github-email@example.com>
2026-06-03 12:57:18 +0530

    location detections working

--- police/lib/screens/detecting_location_screen.dart
+++ police/lib/screens/detecting_location_screen.dart
@@ -1,7 +1,65 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'package:geolocator/geolocator.dart';
 
-class DetectingLocationScreen extends StatelessWidget {
+class DetectingLocationScreen extends StatefulWidget {
   const DetectingLocationScreen({super.key});
+
+  @override
+  State<DetectingLocationScreen> createState() =>
+      _DetectingLocationScreenState();
+}
+
+class _DetectingLocationScreenState extends State<DetectingLocationScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _detectLocation();
+  }
+
+  Future<void> _detectLocation() async {
+    try {
+      print("STEP 1 - Checking permission");
+
+      LocationPermission permission = await Geolocator.checkPermission();
+
+      print("Current Permission: $permission");
+
+      if (permission == LocationPermission.denied) {
+        print("STEP 2 - Requesting permission");
+
+        permission = await Geolocator.requestPermission();
+
+        print("Permission After Request: $permission");
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        print("Permission permanently denied");
+        return;
+      }
+
+      print("STEP 3 - Getting position");
+
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      print("Latitude: ${position.latitude}");
+      print("Longitude: ${position.longitude}");
+
+      print("STEP 4 - Navigating Home");
+
+      if (!mounted) return;
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const HomeScreen()),
+      );
+    } catch (e) {
+      print("LOCATION ERROR: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,20 +71,13 @@ class DetectingLocationScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
 
           children: [
-
-            Image.asset(
-              'assets/images/tn_police_logo.png',
-              height: 100,
-            ),
+            Image.asset('assets/images/tn_police_logo.png', height: 100),
 
             const SizedBox(height: 30),
 
             const Text(
               "Detecting Your Location",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
 
             const SizedBox(height: 15),
